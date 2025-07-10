1eedf0a0062c4c59126bfcdca8118505a409e5fb
waleriosiriano <waleriodev@gmail.com>
2025-07-10 17:06:45 -0300

    Add project files.

---
+++ VendasMvcWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VendasMvcWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
