5209a088a984cbda46ea1d19fb1e6e25a9d8b7c8
waleriosiriano <waleriodev@gmail.com>
2025-07-14 15:30:49 -0300

    Outras Entidades e Segunda Migração

--- VendasMvcWeb/Data/VendasMvcWebContext.cs
+++ VendasMvcWeb/Data/VendasMvcWebContext.cs
@@ -14,6 +14,8 @@ namespace VendasMvcWeb.Data
         {
         }
 
-        public DbSet<VendasMvcWeb.Models.Department> Department { get; set; } = default!;
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
--- VendasMvcWeb/Migrations/20250714182658_OtherEntities.Designer.cs
+++ VendasMvcWeb/Migrations/20250714182658_OtherEntities.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VendasMvcWeb.Data;
 
@@ -10,9 +11,11 @@ using VendasMvcWeb.Data;
 namespace VendasMvcWeb.Migrations
 {
     [DbContext(typeof(VendasMvcWebContext))]
-    partial class VendasMvcWebContextModelSnapshot : ModelSnapshot
+    [Migration("20250714182658_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
