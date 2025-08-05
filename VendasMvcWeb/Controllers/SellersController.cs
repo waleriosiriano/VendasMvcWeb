c230cd35745941e31b1cc95047a46dbda22a1867
waleriosiriano <waleriodev@gmail.com>
2025-08-05 14:00:22 -0300

    Sellers Controller

---
+++ VendasMvcWeb/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VendasMvcWeb.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
