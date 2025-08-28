f8ab0f1a5702d38a8292deba4cb82b96932b6286
waleriosiriano <waleriodev@gmail.com>
2025-08-28 11:51:45 -0300

    Preparing Sales Search Navigation Views

---
+++ VendasMvcWeb/Controllers/SalesRecordsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VendasMvcWeb.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+        public IActionResult GroupinSearch()
+        {
+            return View();
+        }
+    }
+}
