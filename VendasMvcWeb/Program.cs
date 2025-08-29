a49f49605a020ebd6f90381afe1a7f1e7fa41792
waleriosiriano <waleriodev@gmail.com>
2025-08-29 10:55:48 -0300

    Implementing Simple Search

--- VendasMvcWeb/Program.cs
+++ VendasMvcWeb/Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddDbContext<VendasMvcWebContext>(options =>
 builder.Services.AddScoped<SeedingService>();
 builder.Services.AddScoped<SellerService>();
 builder.Services.AddScoped<DepartmentService>();
+builder.Services.AddScoped<SalesRecordService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
---
+++ VendasMvcWeb/Services/SalesRecordService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VendasMvcWeb.Data;
+using VendasMvcWeb.Models;
+
+namespace VendasMvcWeb.Services
+{
+    public class SalesRecordService
+    {
+        private readonly VendasMvcWebContext _context;
+        public SalesRecordService(VendasMvcWebContext context)
+        {
+            _context = context;
+        }
+        public async Task <List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+
+    }
+}
